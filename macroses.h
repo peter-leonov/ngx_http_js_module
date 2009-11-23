3ce65c1c3a84ef0064842f23705bb23abcaeeb77
Peter Leonov <gojpeg@gmail.com>
2009-11-23 03:54:31 +0300

    macrosses depent on NGX_DEBUG

--- macroses.h
+++ macroses.h
@@ -1,6 +1,8 @@
 #ifndef _NGX_HTTP_JS_MACROSES_INCLUDED_
 #define _NGX_HTTP_JS_MACROSES_INCLUDED_
 
+#if (NGX_DEBUG)
+
 #define ngx_assert(expr) \
 if (!(expr)) \
 { \
@@ -8,34 +10,47 @@ if (!(expr)) \
 	ngx_debug_point(); \
 }
 
-
 #define LOG(mess, args...) { fprintf(stderr, mess, ##args); fprintf(stderr, " at %s:%d\n", __FILE__, __LINE__); }
-// #define LOG(mess, args...)
-
-// #define LOG2(mess, args...) { fprintf(stderr, mess, ##args); fprintf(stderr, " at %s:%d\n", __FILE__, __LINE__); }
-#define LOG2(mess, args...)
+#define LOG2(mess, args...) { fprintf(stderr, mess, ##args); fprintf(stderr, " at %s:%d\n", __FILE__, __LINE__); }
 
 // #define TRACE() { fprintf(stderr, "%s() at %s:%d\n", __FUNCTION__, __FILE__, __LINE__); }
 // #define TRACE() { fprintf(stderr, "%s()\n", __FUNCTION__); }
 #define TRACE()
-
-#define TRACE_REQUEST(func) \
-ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s(cx=%p, r=%p)", func, r);
-
-// #define TRACE_REQUEST_METHOD()
-// ngx_log_debug5(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "Request#%s(cx=%p, r=%p, argc=%d, argv=%p)", __FUNCTION__ + 7, cx, r, argc, argv);
-
-#define TRACE_REQUEST_METHOD() \
-ngx_log_debug5(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "Request#%s(r=%p, argc=%d)", __FUNCTION__ + 7, r, argc);
+#define TRACE_REQUEST(func) ngx_log_debug2(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "%s(cx=%p, r=%p)", func, r);
+#define TRACE_REQUEST_METHOD() ngx_log_debug5(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "Request#%s(r=%p, argc=%d)", __FUNCTION__ + 7, r, argc);
 
 #define GET_PRIVATE(private) \
 ngx_assert(cx); \
 ngx_assert(this); \
 if ( (private = JS_GetInstancePrivate(cx, this, private_class, NULL)) == NULL ) \
 { \
-	JS_ReportError(cx, "wrapper object has NULL private pointer in %s\n%s: %u", __FUNCTION__, __FILE__, __LINE__); \
+	JS_ReportError(cx, "wrapper object has NULL private pointer at %s\n%s: %u", __FUNCTION__, __FILE__, __LINE__); \
 	return JS_FALSE; \
 }
+
+
+#else /* NO NGX_DEBUG */
+
+#define ngx_assert(ignore)
+
+#define LOG(mess, args...)
+#define LOG2(mess, args...)
+
+#define TRACE()
+#define TRACE_REQUEST(func)
+#define TRACE_REQUEST_METHOD()
+
+
+#define GET_PRIVATE(private) \
+if ( (private = JS_GetInstancePrivate(cx, this, private_class, NULL)) == NULL ) \
+{ \
+	JS_ReportError(cx, "sorry, the native request had been deleted (see http://wiki.github.com/kung-fu-tzu/ngx_http_js_module/garbagecollecting)"); \
+	return JS_FALSE; \
+}
+
+
+#endif
+
 
 // Just like the throw keyword :)
 #define THROW(mess, args...) { JS_ReportError(cx, mess, ##args); return JS_FALSE; }
