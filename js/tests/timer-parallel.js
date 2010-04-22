f6fab042c5c1167e68b254c65b9f1731df395e2d
Peter Leonov <gojpeg@gmail.com>
2010-04-22 18:18:12 +0400

    restyle

--- js/tests/timer-parallel.js
+++ js/tests/timer-parallel.js
@@ -4,11 +4,11 @@ NginxTests.timerParallel = function (r)
 {
 	function callback ()
 	{
-		r.puts("callback")
+		r.puts('callback')
 		r.flush()
 	}
 	
-	r.sendHttpHeader("text/plain; charset=utf-8")
+	r.sendHttpHeader('text/plain; charset=utf-8')
 	
 	r.setTimer(callback, 500)
 	r.setTimer(callback, 500)
