adf682d37cfce56fe620dd2993aafeffb11bcf21
Peter Leonov <gojpeg@gmail.com>
2010-04-27 16:37:07 +0400

    real tests for Nginx.time

--- js/tests/time.js
+++ js/tests/time.js
@@ -2,7 +2,39 @@
 
 NginxTests.time = function (r)
 {
-	r.sendString(+new Date() + ", " + Nginx.time + "\n")
+	r.sendHttpHeader('text/plain; charset=utf-8')
+	
+	Tests.test('tests for Nginx object', function (t)
+	{
+		t.istypeof(Nginx, 'Nginx present')
+		
+		t.test('Nginx.time', function (t)
+		{
+			var timerPrecision = 250
+			
+			var jmsec = +new Date(),
+				nmsec = Nginx.time
+			
+			t.peq(jmsec, nmsec, timerPrecision, 'new Date() == Nginx.time')
+			
+			function later (e)
+			{
+				var ljmsec = +new Date(),
+					lnmsec = Nginx.time
+				
+				t.peq(ljmsec, lnmsec, timerPrecision, 'new Date() == Nginx.time')
+				t.peq(ljmsec - jmsec, timerPrecision * 4, timerPrecision, 'diff within one second')
+			}
+			t.async(later, timerPrecision * 4)
+		})
+		
+	})
+	Tests.oncomplete = function ()
+	{
+		r.sendSpecial(Nginx.HTTP_LAST)
+	}
+	Tests.run(r)
+	
 	return Nginx.OK
 }
 
