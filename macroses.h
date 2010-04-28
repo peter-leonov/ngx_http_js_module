6bffbf8a790b3824d569b53aae2dc26a6afda89b
Peter Leonov <gojpeg@gmail.com>
2010-04-28 16:32:51 +0400

    more ANSI colors added

--- macroses.h
+++ macroses.h
@@ -2,13 +2,31 @@
 #define _NGX_HTTP_JS_MACROSES_INCLUDED_
 
 #if (NGX_HTTP_JS_COLORED)
-#define COLOR_RED   "\x1B[31m"
-#define COLOR_GREEN "\x1B[32m"
-#define COLOR_CLEAR "\x1B[0m"
+#define COLOR_CLEAR   "\x1B[0m"
+#define COLOR_BRIGHT  "\x1B[1m"
+#define COLOR_FAINT   "\x1B[2m"
+
+#define COLOR_BLACK   "\x1B[30m"
+#define COLOR_RED     "\x1B[31m"
+#define COLOR_GREEN   "\x1B[32m"
+#define COLOR_YELLOW  "\x1B[33m"
+#define COLOR_BLUE    "\x1B[34m"
+#define COLOR_MAGENTA "\x1B[35m"
+#define COLOR_CYAN    "\x1B[36m"
+#define COLOR_WHITE   "\x1B[37m"
 #else
-#define COLOR_RED   ""
-#define COLOR_GREEN ""
-#define COLOR_CLEAR ""
+#define COLOR_CLEAR   ""
+#define COLOR_BRIGHT  ""
+#define COLOR_FAINT   ""
+
+#define COLOR_BLACK   ""
+#define COLOR_RED     ""
+#define COLOR_GREEN   ""
+#define COLOR_YELLOW  ""
+#define COLOR_BLUE    ""
+#define COLOR_MAGENTA ""
+#define COLOR_CYAN    ""
+#define COLOR_WHITE   ""
 #endif
 
 #if (NGX_DEBUG)
