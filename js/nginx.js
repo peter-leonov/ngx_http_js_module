3e18eb6a4f1cca31bc70feb6847d7ed443fc7787
Peter Leonov <gojpeg@gmail.com>
2010-04-28 16:34:27 +0400

    colored JS log messages

--- js/nginx.js
+++ js/nginx.js
@@ -1,3 +1,27 @@
+;(function(){
+
+self.ANSIColors =
+{
+	CLEAR:   '\x1B[0m',
+	BRIGHT:  '\x1B[1m',
+	FAINT:   '\x1B[2m',
+	BLACK:   '\x1B[30m',
+	RED:     '\x1B[31m',
+	GREEN:   '\x1B[32m',
+	YELLOW:  '\x1B[33m',
+	BLUE:    '\x1B[34m',
+	MAGENTA: '\x1B[35m',
+	CYAN:    '\x1B[36m',
+	WHITE:   '\x1B[37m',
+	
+	wrap: function (str, color)
+	{
+		return this[color] + str + this.CLEAR
+	}
+}
+
+})();
+
 // Nginx wrapers
 ;(function(){
 	if (!self.Nginx)
@@ -5,10 +29,10 @@
 	
 	var Me = self.Nginx
 	function flat (args) { return Array.prototype.slice.apply(args).join(", ") }
-	Me.log   = function () { this.logError(this.LOG_WARN,  flat(arguments)) } // LOG_DEBUG
-	Me.info  = function () { this.logError(this.LOG_INFO,  flat(arguments)) }
-	Me.warn  = function () { this.logError(this.LOG_WARN,  flat(arguments)) }
-	Me.error = function () { this.logError(this.LOG_ERR,   flat(arguments)) }
+	Me.log   = function () { this.logError(this.LOG_WARN, ANSIColors.wrap(flat(arguments), 'GREEN')) } // LOG_DEBUG
+	Me.info  = function () { this.logError(this.LOG_INFO, ANSIColors.wrap(flat(arguments), 'CLEAR')) }
+	Me.warn  = function () { this.logError(this.LOG_WARN, ANSIColors.wrap(flat(arguments), 'CLEAR')) }
+	Me.error = function () { this.logError(this.LOG_ERR,  ANSIColors.wrap(flat(arguments), 'CLEAR')) }
 	self.log = function () { Me.log.apply(Me, arguments) }
 	
 	Me.resultNames = {}
