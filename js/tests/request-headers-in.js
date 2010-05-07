2c34c685e1252d8e6de6036d9917cbb2a42a1d5b
Peter Leonov <gojpeg@gmail.com>
2010-05-07 12:12:27 +0400

    flexible tests for r.headersIn

---
+++ js/tests/request-headers-in.js
@@ -0,0 +1,30 @@
+;(function(){
+
+NginxTests.requestHeadersIn = function (r)
+{
+	r.sendHttpHeader('text/plain; charset=utf-8')
+	
+	var args = JSON.parse(r.args)
+	
+	Tests.test('tests for request headers processing', function (t)
+	{
+		var h = r.headersIn
+		
+		t.ok(args, 'args')
+		t.ok(h, 'headersIn')
+		
+		for (var k in args)
+			t.eq(h[k], args[k], k)
+	})
+	Tests.oncomplete = function ()
+	{
+		r.puts('all done')
+		r.flush()
+		r.sendSpecial(Nginx.HTTP_LAST)
+	}
+	Tests.run(r)
+	
+	return Nginx.OK
+}
+
+})();
