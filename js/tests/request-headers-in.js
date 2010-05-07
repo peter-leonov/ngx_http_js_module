ee281a07af0cb1ca9aaaa92cd3d2783cf1fbfaf1
Peter Leonov <gojpeg@gmail.com>
2010-05-07 18:07:27 +0400

    test the case insensitiveness

--- js/tests/request-headers-in.js
+++ js/tests/request-headers-in.js
@@ -14,7 +14,10 @@ NginxTests.requestHeadersIn = function (r)
 		t.ok(h, 'headersIn')
 		
 		for (var k in args)
+		{
 			t.eq(typeof args[k] == 'number' ? +h[k] : h[k], args[k], k)
+			t.eq(typeof args[k] == 'number' ? +h[k] : h[k.toUpperCase()], args[k], k.toUpperCase())
+		}
 	})
 	Tests.oncomplete = function ()
 	{
