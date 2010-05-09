7500772ff45978fd1bdd140072774bcc063e3291
Peter Leonov <gojpeg@gmail.com>
2010-05-09 16:45:34 +0400

    simple test for js_set

---
+++ js/tests/variable-set.js
@@ -0,0 +1,22 @@
+;(function(){
+
+NginxTests.variableSet = function (r)
+{
+	r.sendHttpHeader('text/plain; charset=utf-8')
+	
+	var args = UrlEncode.parse(r.args)
+	
+	Tests.test('tests for js_set', function (t)
+	{
+		t.eq(args.foo, 'bar', 'args.foo')
+	})
+	Tests.oncomplete = function ()
+	{
+		r.sendSpecial(Nginx.HTTP_LAST)
+	}
+	Tests.run(r)
+	
+	return Nginx.OK
+}
+
+})();
