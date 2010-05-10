17e7b63f28699beead826ec4df29cea32e2a00e1
Peter Leonov <gojpeg@gmail.com>
2010-05-10 22:05:57 +0400

    reading the nginx variables works

--- src/classes/Request/Variables.c
+++ src/classes/Request/Variables.c
@@ -84,25 +84,59 @@ static JSBool
 getProperty(JSContext *cx, JSObject *self, jsval id, jsval *vp)
 {
 	ngx_http_request_t         *r;
+	ngx_str_t                   var;
+	ngx_uint_t                  hash;
+	ngx_http_variable_value_t  *vv;
+	JSString                   *key_jss;
+	u_char                     *key, *lowcase;
+	size_t                      len;
 	
 	TRACE();
 	GET_PRIVATE(r);
 	
-	if (JSVAL_IS_STRING(id))
+	key_jss = JS_ValueToString(cx, id);
+	if (key_jss == NULL)
 	{
-		char        *name;
-		
-		name = JS_GetStringBytes(JSVAL_TO_STRING(id));
-		if (name == NULL)
-		{
-			JS_ReportError(cx, "can't get the C string of the property name");
-			return JS_FALSE;
-		}
-		
-		ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "variables[\"%s\"]", name);
-		
-		// NGX_STRING_to_JS_STRING_to_JSVAL(cx, cookie_value, *vp);
+		return JS_FALSE;
 	}
+	
+	key = (u_char *) JS_GetStringBytes(key_jss);
+	if (key == NULL)
+	{
+		JS_ReportError(cx, "can't get the C string of the property name");
+		return JS_FALSE;
+	}
+	
+	ngx_log_debug1(NGX_LOG_DEBUG_HTTP, r->connection->log, 0, "variables[\"%s\"]", key);
+	
+	len = ngx_strlen(key);
+	
+	lowcase = ngx_pnalloc(r->pool, len);
+	if (lowcase == NULL)
+	{
+		JS_ReportOutOfMemory(cx);
+		return JS_FALSE;
+	}
+	
+	hash = ngx_hash_strlow(lowcase, key, len);
+	
+	var.len = len;
+	var.data = lowcase;
+	
+	vv = ngx_http_get_variable(r, &var, hash);
+	if (vv == NULL)
+	{
+		JS_ReportOutOfMemory(cx);
+		return JS_FALSE;
+	}
+	
+	if (vv->not_found)
+	{
+		*vp = JSVAL_VOID;
+		return JS_TRUE;
+	}
+	
+	NGX_STRING_to_JS_STRING_to_JSVAL(cx, *vv, *vp);
 	
 	return JS_TRUE;
 }
