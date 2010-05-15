0a427949257bc779a469c66e0a5c61dfccf73832
Peter Leonov <gojpeg@gmail.com>
2010-05-15 20:00:37 +0400

    subrequest to the JS handler

--- js/tests/subrequests.js
+++ js/tests/subrequests.js
@@ -4,30 +4,48 @@ NginxTests.subrequests = function (r)
 {
 	r.sendHttpHeader('text/plain; charset=utf-8')
 	
-	Tests.test('tests for subrequests', function (t)
-	{
-		function callback (body, rc)
+	// Tests.test('tests for subrequests', function (t)
+	// {
+		function callback (sr, body, rc)
 		{
-			log('\x1B[31m!!!\x1B[0m')
-			body = String(body)
-			// r.print('callback with body="' + body + '", length=' + body.length + '\n')
-			// r.flush()
-			
-			t.done()
+			log('\x1B[31m!!!\x1B[0m ' + rc)
+			var body = String(body)
+			r.puts('callback with rc=' + rc + ', body=' + JSON.stringify(body) + ', header=' + sr.headersOut['X-Lalala'])
+			r.flush()
+			r.sendSpecial(Nginx.HTTP_LAST)
+			// t.done()
 		}
 		
 		// r.subrequest('/quick', callback)
+		// r.subrequest('/slow', callback)
+		r.subrequest('/run/subrequests-headers', callback)
 		
-		t.wait(3000)
-	})
-	Tests.oncomplete = function ()
-	{
-		r.puts('all done')
-		r.flush()
-		r.sendSpecial(Nginx.HTTP_LAST)
-	}
-	Tests.run(r)
+		// t.wait(3000)
+	// })
+	// Tests.oncomplete = function ()
+	// {
+	// 	r.puts('all done')
+	// 	r.flush()
+	// 	r.sendSpecial(Nginx.HTTP_LAST)
+	// }
+	// Tests.run(r)
 	
+	return Nginx.OK
+}
+
+NginxTests.subrequestsHeaders = function (r)
+{
+	r.headersOut['X-Lalala'] = 'lololo'
+	r.sendHttpHeader('text/plain; charset=utf-8')
+	r.print('the body')
+	r.flush()
+	r.print('the body')
+	r.flush()
+	r.print('the body')
+	r.flush()
+	r.sendSpecial(Nginx.HTTP_LAST)
+	
+	// return Nginx.DONE to avoid subrequest handler
 	return Nginx.OK
 }
 
