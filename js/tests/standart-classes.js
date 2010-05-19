7f69d550147676376c1820ba312ed8541ea2db7e
Peter Leonov <gojpeg@gmail.com>
2010-05-19 20:12:12 +0400

    proper test name

--- js/tests/standart-classes.js
+++ js/tests/standart-classes.js
@@ -4,7 +4,7 @@ NginxTests.standartClasses = function (r)
 {
 	r.sendHttpHeader('text/plain; charset=utf-8')
 	
-	Tests.test('tests for Nginx object', function (t)
+	Tests.test('tests for standart classes extensions', function (t)
 	{
 		t.eq('123'.utf8length, 3, '"123".utf8length')
 		t.eq('xxxccc'.utf8length, 6, '"xxxccc".utf8length')
