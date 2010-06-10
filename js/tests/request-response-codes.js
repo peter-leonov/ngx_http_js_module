a768be6c27f618a969621effd94be07e53b23891
Peter Leonov <gojpeg@gmail.com>
2010-06-10 14:58:17 +0400

    descriptive names

--- js/tests/request-response-codes.js
+++ js/tests/request-response-codes.js
@@ -6,16 +6,21 @@ NginxTests.requestResponseCodes = function (r)
 	
 	Tests.test('tests for body callback result codes', function (t)
 	{
-		function test (code)
+		function test (name, res)
 		{
-			t.test('rc = ' + code, function (t)
+			var code = Nginx[name]
+			
+			if (res === undefined)
+				res = code
+			
+			t.test(name + ' (' + code + ')', function (t)
 			{
 				function callback (sr, body, rc)
 				{
 					if (rc != Nginx.OK)
 						return
 					
-					t.eq(sr.headersOut.status, code, 'response status')
+					t.eq(sr.headersOut.status, res, 'response status')
 					
 					t.done()
 				}
@@ -26,10 +31,24 @@ NginxTests.requestResponseCodes = function (r)
 			})
 		}
 		
-		test(403)
-		test(404)
+		test('HTTP_NO_CONTENT')
+		test('HTTP_MOVED_PERMANENTLY')
+		test('HTTP_MOVED_TEMPORARILY')
+		test('HTTP_NOT_MODIFIED')
 		
+		test('HTTP_BAD_REQUEST')
+		test('HTTP_UNAUTHORIZED')
+		test('HTTP_FORBIDDEN')
+		test('HTTP_NOT_FOUND')
+		test('HTTP_NOT_ALLOWED')
+		test('HTTP_CLOSE', 502)
 		
+		test('HTTP_INTERNAL_SERVER_ERROR')
+		test('HTTP_NOT_IMPLEMENTED')
+		test('HTTP_BAD_GATEWAY')
+		test('HTTP_SERVICE_UNAVAILABLE')
+		test('HTTP_GATEWAY_TIME_OUT')
+		test('HTTP_INSUFFICIENT_STORAGE')
 	})
 	Tests.oncomplete = function ()
 	{
