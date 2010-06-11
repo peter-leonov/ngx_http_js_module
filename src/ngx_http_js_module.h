dd0c71bcb280201191d24b1ffbeed9a3c809705a
Peter Leonov <gojpeg@gmail.com>
2010-06-11 18:16:16 +0400

    restyle

--- src/ngx_http_js_module.h
+++ src/ngx_http_js_module.h
@@ -3,7 +3,8 @@
 
 typedef ngx_http_request_t   *nginx;
 
-typedef struct {
+typedef struct
+{
 	ngx_str_t                 redirect_uri;
 	ngx_str_t                 redirect_args;
 	
@@ -18,28 +19,34 @@ typedef struct {
 	ngx_chain_t              *chain_first, *chain_last;
 	
 	ngx_event_t               js_timer;
-} ngx_http_js_ctx_t;
+}
+ngx_http_js_ctx_t;
 
 
-typedef struct {
+typedef struct
+{
 	JSObject                  *content_handler_function;
 	ngx_str_t                  content_handler_name;
 	JSObject                  *access_handler_function;
 	ngx_str_t                  access_handler_name;
-} ngx_http_js_loc_conf_t;
+}
+ngx_http_js_loc_conf_t;
 
 
-typedef struct{
+typedef struct
+{
 	size_t                     maxmem;
 	ngx_array_t                requires, loads;
-} ngx_http_js_main_conf_t;
+}
+ngx_http_js_main_conf_t;
 
 
 typedef struct
 {
 	JSObject                  *function;
 	ngx_str_t                  handler;
-} ngx_http_js_variable_t;
+}
+ngx_http_js_variable_t;
 
 
 extern ngx_http_output_header_filter_pt  ngx_http_js_next_header_filter;
