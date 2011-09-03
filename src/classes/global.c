aeaa00c3d5429e5c0dd4925ff8a0f564a7648f68
Peter Leonov <gojpeg@gmail.com>
2011-09-03 04:26:18 +0400

    strict stub

--- src/classes/global.c
+++ src/classes/global.c
@@ -128,7 +128,7 @@ getter_utf8length(JSContext *cx, JSObject *self, jsval id, jsval *vp)
 static JSClass global_class =
 {
 	"global", JSCLASS_GLOBAL_FLAGS,
-	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_PropertyStub,
+	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_StrictPropertyStub,
 	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, JS_FinalizeStub,
 	JSCLASS_NO_OPTIONAL_MEMBERS
 };
