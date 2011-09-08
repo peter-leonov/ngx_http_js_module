b3fa10ff925698372682c10b82ed74672544c8cf
Peter Leonov <gojpeg@gmail.com>
2011-09-08 04:19:07 +0400

    long getter -> properties and dedicated getters

--- src/classes/File.c
+++ src/classes/File.c
@@ -436,35 +436,21 @@ method_close(JSContext *cx, uintN argc, jsval *vp)
 
 
 static JSBool
-getProperty(JSContext *cx, JSObject *self, jsval id, jsval *vp)
+getter_size(JSContext *cx, JSObject *self, jsid id, jsval *vp)
 {
-	ngx_fd_t         fd;
-	void            *p;
+	ngx_fd_t          fd;
+	void             *p;
+	ngx_file_info_t   fi;
+	off_t             size;
 	
 	GET_PRIVATE(p);
 	fd = PTR_TO_FD(p);
 	TRACE();
 	
-	if (JSVAL_IS_INT(id))
+	if (ngx_fd_info(fd, &fi) != NGX_FILE_ERROR)
 	{
-		switch (JSVAL_TO_INT(id))
-		{
-			case 1:
-			{
-				ngx_file_info_t   fi;
-				off_t             size;
-				
-				if (ngx_fd_info(fd, &fi) != NGX_FILE_ERROR)
-				{
-					size = ngx_file_size(&fi);
-					if (!JS_NewNumberValue(cx, size, vp))
-					{
-						return JS_FALSE;
-					}
-				}
-			}
-			break;
-		}
+		size = ngx_file_size(&fi);
+		*vp = DOUBLE_TO_JSVAL(size);
 	}
 	
 	return JS_TRUE;
@@ -509,39 +495,21 @@ finalizer(JSContext *cx, JSObject *self)
 }
 
 static JSBool
-static_getProperty(JSContext *cx, JSObject *self, jsval id, jsval *vp)
+getter_readLeaks(JSContext *cx, JSObject *self, jsid id, jsval *vp)
 {
 	TRACE();
 	
-	// LOG("Nginx property id = %d\n", JSVAL_TO_INT(id));
-	if (JSVAL_IS_INT(id))
-	{
-		switch (JSVAL_TO_INT(id))
-		{
-			case 1:   *vp = INT_TO_JSVAL(NGX_INVALID_FILE); break;
-			case 2:   *vp = INT_TO_JSVAL(NGX_FILE_ERROR); break;
-			
-#ifdef NGX_HAVE_CASELESS_FILESYSTEM
-			case 3:   *vp = JSVAL_TRUE; break;
-#else
-			case 3:   *vp = JSVAL_FALSE; break;
-#endif
-			
-			case 4:   *vp = INT_TO_JSVAL(NGX_FILE_RDONLY); break;
-			case 5:   *vp = INT_TO_JSVAL(NGX_FILE_WRONLY); break;
-			case 6:   *vp = INT_TO_JSVAL(NGX_FILE_RDWR); break;
-			case 7:   *vp = INT_TO_JSVAL(NGX_FILE_CREATE_OR_OPEN); break;
-			case 8:   *vp = INT_TO_JSVAL(NGX_FILE_OPEN); break;
-			case 9:   *vp = INT_TO_JSVAL(NGX_FILE_TRUNCATE); break;
-			case 10:  *vp = INT_TO_JSVAL(NGX_FILE_APPEND); break;
-			case 11:  *vp = INT_TO_JSVAL(NGX_FILE_NONBLOCK); break;
-			case 12:  *vp = INT_TO_JSVAL(NGX_FILE_DEFAULT_ACCESS); break;
-			case 13:  *vp = INT_TO_JSVAL(NGX_FILE_OWNER_ACCESS); break;
-			
-			case 100: *vp = INT_TO_JSVAL(read_leaks); break;
-			case 101: *vp = INT_TO_JSVAL(open_files); break;
-		}
-	}
+	*vp = INT_TO_JSVAL(read_leaks);
+	
+	return JS_TRUE;
+}
+
+static JSBool
+getter_openFiles(JSContext *cx, JSObject *self, jsid id, jsval *vp)
+{
+	TRACE();
+	
+	*vp = INT_TO_JSVAL(open_files);
 	
 	return JS_TRUE;
 }
@@ -557,7 +525,7 @@ static JSFunctionSpec funcs[] =
 
 static JSPropertySpec props[] =
 {
-	{"size",                     1,  JSPROP_READONLY, NULL, NULL},
+	{"size",  0,  JSPROP_READONLY, getter_size, NULL},
 	{0, 0, 0, NULL, NULL}
 };
 
@@ -575,25 +543,8 @@ static JSFunctionSpec static_funcs[] =
 
 static JSPropertySpec static_props[] =
 {
-	{"INVALID",                  1,   JSPROP_READONLY, static_getProperty, NULL},
-	{"ERROR",                    2,   JSPROP_READONLY, static_getProperty, NULL},
-	
-	{"HAVE_CASELESS_FILESYSTEM", 3,   JSPROP_READONLY, static_getProperty, NULL},
-	
-	{"RDONLY",                   4,   JSPROP_READONLY, static_getProperty, NULL},
-	{"WRONLY",                   5,   JSPROP_READONLY, static_getProperty, NULL},
-	{"RDWR",                     6,   JSPROP_READONLY, static_getProperty, NULL},
-	{"CREATE_OR_OPEN",           7,   JSPROP_READONLY, static_getProperty, NULL},
-	{"OPEN",                     8,   JSPROP_READONLY, static_getProperty, NULL},
-	{"TRUNCATE",                 9,   JSPROP_READONLY, static_getProperty, NULL},
-	{"APPEND",                   10,  JSPROP_READONLY, static_getProperty, NULL},
-	{"NONBLOCK",                 11,  JSPROP_READONLY, static_getProperty, NULL},
-	{"DEFAULT_ACCESS",           12,  JSPROP_READONLY, static_getProperty, NULL},
-	{"OWNER_ACCESS",             13,  JSPROP_READONLY, static_getProperty, NULL},
-	
-	{"readLeaks",                100, JSPROP_READONLY, static_getProperty, NULL},
-	{"openFiles",                101, JSPROP_READONLY, static_getProperty, NULL},
-	
+	{"readLeaks",    0, JSPROP_READONLY, getter_readLeaks, NULL},
+	{"openFiles",    0, JSPROP_READONLY, getter_openFiles, NULL},
 	{0, 0, 0, NULL, NULL}
 };
 
@@ -601,7 +552,7 @@ JSClass ngx_http_js__nginx_file__class =
 {
 	"File",
 	JSCLASS_HAS_PRIVATE,
-	JS_PropertyStub, JS_PropertyStub, getProperty, JS_PropertyStub,
+	JS_PropertyStub, JS_PropertyStub, JS_PropertyStub, JS_StrictPropertyStub,
 	JS_EnumerateStub, JS_ResolveStub, JS_ConvertStub, finalizer,
 	JSCLASS_NO_OPTIONAL_MEMBERS
 };
@@ -610,7 +561,7 @@ JSBool
 ngx_http_js__nginx_file__init(JSContext *cx, JSObject *global)
 {
 	static int  inited = 0;
-	JSObject    *nginxobj;
+	JSObject    *nginxobj, *fileobj;
 	jsval        vp;
 	
 	if (inited)
@@ -625,6 +576,57 @@ ngx_http_js__nginx_file__init(JSContext *cx, JSObject *global)
 	ngx_http_js__nginx_file__prototype = JS_InitClass(cx, nginxobj, NULL, &ngx_http_js__nginx_file__class,  constructor, 0,
 		props, funcs, static_props, static_funcs);
 	E(ngx_http_js__nginx_file__prototype, "Can`t JS_InitClass(Nginx.File)");
+	
+	E(JS_GetProperty(cx, nginxobj, "File", &vp), "global.Nginx.File is undefined");
+	fileobj = JSVAL_TO_OBJECT(vp);
+	
+	
+	
+	vp = INT_TO_JSVAL(NGX_FILE_RDONLY);
+	JS_SetProperty(cx, fileobj, "RDONLY", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_INVALID_FILE);
+	JS_SetProperty(cx, fileobj, "INVALID", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_ERROR);
+	JS_SetProperty(cx, fileobj, "ERROR", &vp);
+	
+#ifdef NGX_HAVE_CASELESS_FILESYSTEM
+	vp = JSVAL_TRUE;
+#else
+	vp = JSVAL_FALSE;
+#endif
+	JS_SetProperty(cx, fileobj, "HAVE_CASELESS_FILESYSTEM", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_RDONLY);
+	JS_SetProperty(cx, fileobj, "RDONLY", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_WRONLY);
+	JS_SetProperty(cx, fileobj, "WRONLY", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_RDWR);
+	JS_SetProperty(cx, fileobj, "RDWR", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_CREATE_OR_OPEN);
+	JS_SetProperty(cx, fileobj, "CREATE_OR_OPEN", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_OPEN);
+	JS_SetProperty(cx, fileobj, "OPEN", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_TRUNCATE);
+	JS_SetProperty(cx, fileobj, "TRUNCATE", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_APPEND);
+	JS_SetProperty(cx, fileobj, "APPEND", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_NONBLOCK);
+	JS_SetProperty(cx, fileobj, "NONBLOCK", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_DEFAULT_ACCESS);
+	JS_SetProperty(cx, fileobj, "DEFAULT_ACCESS", &vp);
+	
+	vp = INT_TO_JSVAL(NGX_FILE_OWNER_ACCESS);
+	JS_SetProperty(cx, fileobj, "OWNER_ACCESS", &vp);
 	
 	inited = 1;
 	
