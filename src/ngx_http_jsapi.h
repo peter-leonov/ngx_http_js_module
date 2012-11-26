32a0805032a124f616e14002717db99ef7ae8a6b
Peter Leonov <gojpeg@gmail.com>
2012-11-26 18:43:55 +0400

    debug doesn't link on Lion

--- src/ngx_http_jsapi.h
+++ src/ngx_http_jsapi.h
@@ -1,10 +1,10 @@
 #ifndef _NGX_HTTP_JSAPI_H_INCLUDED_
 #define _NGX_HTTP_JSAPI_H_INCLUDED_
 
-// needded for JS_SetGCZeal()
-#ifdef NGX_DEBUG
-#define DEBUG
-#endif
+// // needded for JS_SetGCZeal()
+// #ifdef NGX_DEBUG
+// #define DEBUG
+// #endif
 
 #include <js/jsapi.h>
 
